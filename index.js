3364027b628a4cbe146591a24e76a2316cf7f532
roza <rozanawzad2001@gmail.com>
2022-12-12 20:34:57 +0300

    done js array find lab

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // code your solution here
+const record = [
+    {year: "2018", result: "L"},
+    {year: "2017", result: "W"},
+    {year: "2016", result: "N/A"}
+    //...
+]
+
+const superbowlWin = (record) => {
+
+    const result = record.find((l) => {
+        if(l.result === "W") {
+            return true
+        } 
+        else { 
+            return false 
+        }
+    })
+
+    if(result) { 
+        return result.year 
+    } else {
+         return undefined 
+        }
+
+}
